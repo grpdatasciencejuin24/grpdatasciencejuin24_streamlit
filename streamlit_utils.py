207468f28e41528474969cf1a45ee23ec58c8685
vkomiwes <vincent.komiwes@gmail.com>
2025-03-29 17:44:48 +0100

    Add files via upload

--- streamlit_utils.py
+++ streamlit_utils.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os 
+import base64
 import folium
 from streamlit_folium import st_folium
 
@@ -12,11 +13,58 @@ css_code = """
 def write_text(text):
     st.write(text)
 
-def banner():
-    #with st.container(mode="fixed", position="bottom", border=True):
-    st.image("img/Image1.png")
+def banner2(val):
+
+    match val:
+        case "neural_network":
+            set_bg_hack("img/neural_network.png")
+        case "accident":
+            set_bg_hack("img/accident.png")
+        case "data_analyse":
+            set_bg_hack("img/data_analyse.png")
+        case "machine_learning":
+            set_bg_hack("img/machine_learning.png")
+        case _:
+            set_bg_hack("img/accident.png")
+    return
     
+
+def banner(val):
+
+    match val:
+        case "neural_network":
+            set_bg_hack("img/accident.png")
+        case "accident":
+            set_bg_hack("img/accident.png")
+        case "data_analyse":
+            set_bg_hack("img/accident.png")
+        case "machine_learning":
+            set_bg_hack("img/machine_learning.png")
+        case _:
+            set_bg_hack("img/machine_learning.png")
+    return
+
+def set_bg_hack2(main_bg):
+ 
+    main_bg_ext = "png"
+
+def set_bg_hack(main_bg):
+ 
+    main_bg_ext = "png"
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
     
+
 def map_view(lat,long,vcolor, popup_message):
 
     map_center = [lat, long]
