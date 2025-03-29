fb9f60b172b7d4ba332d59ccd66291bf19ba52f6
vkomiwes <vincent.komiwes@gmail.com>
2025-03-29 18:39:42 +0100

    Add files via upload

---
+++ streamlit_param.py
@@ -0,0 +1,10 @@
+
+# Begin Sydney 05/11/2024
+
+# Chemin où se trouvent les Notebooks : à modifier 
+
+# -------------------------------------------------------
+PATH = 'C:\\Python\\Datascientest\\Projet\\8-Streamlit\\'
+# -------------------------------------------------------
+
+# End Sydney 05/11/2024
