f545b5c26ed82c389afdc383c39b751056026d25
vkomiwes <vincent.komiwes@gmail.com>
2025-03-31 20:22:18 +0200

    Add files via upload

--- 5_modelisation_nbacc/approche_resultats_nbacc.py
+++ 5_modelisation_nbacc/approche_resultats_nbacc.py
@@ -7,103 +7,121 @@ st.markdown(css_code, unsafe_allow_html=True)
 
 st.title("Modélisation du nombre d'accidents")
 
-st.header("Approche utilisée")
 
-text = """
-LSTM (Long Short-Term Memory) est une méthode de Deep learning très efficace pour traiter les séries temporelles. Nous souhaitons la mettre à profit pour concevoir un modèle, l’évaluer, et sous réserve de performances acceptables, l’utiliser pour prédire le nombre d’accidents annuels.
-Les modèles de mémoire à long terme LTSM sont un type d'architecture de réseau neuronal récurrent (RNN)*. Ils ont récemment acquis une importance significative dans le domaine du Deep Learning.
+def tab_approche():
 
-* Structure des RNN : Contrairement aux réseaux de neurones classiques (comme les perceptrons multicouches), un RNN a une structure de boucle qui permet aux informations de circuler non seulement d'un neurone à un autre, mais aussi à travers les étapes du temps. Chaque sortie d'une cellule de RNN à un moment donné devient l'entrée pour le même neurone à l'instant suivant, ce qui permet au réseau de garder une mémoire des événements passés.
+    st.header("Approche")
 
-LSTM repose sur trois portes principales : la porte d'entrée (input gate), la porte d'oubli (forget gate) et la porte de sortie (output gate).  
-"""
-write_text(text)
+    text = """
+    LSTM (Long Short-Term Memory) est une méthode de Deep learning très efficace pour traiter les séries temporelles. Nous souhaitons la mettre à profit pour concevoir un modèle, l’évaluer, et sous réserve de performances acceptables, l’utiliser pour prédire le nombre d’accidents annuels.
+    Les modèles de mémoire à long terme LTSM sont un type d'architecture de réseau neuronal récurrent (RNN). Ils ont récemment acquis une importance significative dans le domaine du Deep Learning.
+    \n
+    Contrairement aux réseaux de neurones classiques (comme les perceptrons multicouches), un RNN a une structure de boucle qui permet aux informations de circuler non seulement d'un neurone à un autre, mais aussi à travers les étapes du temps. Chaque sortie d'une cellule de RNN à un moment donné devient l'entrée pour le même neurone à l'instant suivant, ce qui permet au réseau de garder une mémoire des événements passés.
 
-image_path = "img/image086.png" 
-st.image(image_path, width=500)
+    LSTM repose sur trois portes principales : la porte d'entrée (input gate), la porte d'oubli (forget gate) et la porte de sortie (output gate).  
+    """
+    write_text(text)
 
-
-text = """
-Pour comparer les différents paramètres, nous allons utiliser l’approche représentée par le schéma ci-dessous :
-"""
-write_text(text)
-
-image_path = "img/image087.png" 
-st.image(image_path, width=1200)
+    image_path = "img/image086.png" 
+    st.image(image_path, width=500)
 
 
-st.header("Résultats")
+    text = """
+    Pour évaluer les performances du modèle, nous allons utiliser des métriques spécifiques aux séries temporelles en évaluant la qualité de la précision avec le Mean Absolute Percentage Error (MAPE) et le Symmetric Mean Absolute Percentage Error (sMAPE).
+    lus ces valeurs sont faibles (proche de 0), meilleure est la prédiction.
+    """
+    st.write(text)
 
-st.subheader("Baseline")
+    text = """
+    Pour comparer les différents paramètres, nous allons utiliser l’approche représentée par le schéma ci-dessous :
+    """
+    write_text(text)
 
-text = """
-Nous observons une convergence de la fonction de perte (Loss) vers 0, aussi bien sur l’ensemble d’entraînement que sur l’ensemble de test, avec les paramètres ci-dessous (voir graphique).
-Une erreur tendant vers 0 sur l’ensemble d’entraînement indique que le modèle apprend efficacement et que la précision augmente durant l’apprentissage. De même, une erreur proche de 0 sur l’ensemble de test suggère que le modèle généralise bien sur des données nouvelles qu’il n’a jamais vues, sans signe d’overfitting, ce qui est très encourageant.
-"""
+    image_path = "img/image087.png" 
+    st.image(image_path, width=1200)
 
-st.write(text)
+    return
 
-image_path = "img/image091.png" 
-st.image(image_path, width=600)
+def tab_resultats():
 
+    st.header("Résultats")
 
-st.subheader("Métriques utilisées")
+    st.subheader("Baseline")
 
-text = """
-Pour évaluer les performances du modèle pour la baseline, nous allons utiliser des métriques spécifiques aux séries temporelles en évaluant la qualité de la précision avec le Mean Absolute Percentage Error (MAPE) et le Symmetric Mean Absolute Percentage Error (sMAPE).
-lus ces valeurs sont faibles (proche de 0), meilleure est la prédiction.
-Nous obtenons les valeurs suivantes qui sont très satisfaisantes sur les ensembles de training et de test :
-\n - MAPE sur l’ensemble de training: 14.12%
-\n - sMAPE sur l’ensemble de training : 13.42%
-\n - MAPE sur l’ensemble de test : 12.67%
-\n - sMAPE sur l’ensemble de test : 12.22%
-"""
-st.write(text)
+    text = """
+    Nous observons une convergence de la fonction de perte (Loss) vers 0, aussi bien sur l’ensemble d’entraînement que sur l’ensemble de test, avec les paramètres ci-dessous (voir graphique).
+    Une erreur tendant vers 0 sur l’ensemble d’entraînement indique que le modèle apprend efficacement et que la précision augmente durant l’apprentissage. De même, une erreur proche de 0 sur l’ensemble de test suggère que le modèle généralise bien sur des données nouvelles qu’il n’a jamais vues, sans signe d’overfitting, ce qui est très encourageant.
+    """
 
-image_path = "img/image094.jpg" 
-st.image(image_path, width=1000)
+    st.write(text)
 
-st.subheader("Tuning des Hyperparamètres et performances finales")
+    image_path = "img/image091.png" 
+    st.image(image_path, width=600)
 
-text = """
-Nous avons utilisé le dictionnaire ci-dessous de paramètres pour effectuer le tuning des hyperparamètres :
-"""
-st.write(text)
+    text = """
+    Nous obtenons les valeurs suivantes d'évaluation des métriques, très satisfaisantes sur les ensembles de training et de test :
+    \n - MAPE sur l’ensemble de training: 14.12%
+    \n - sMAPE sur l’ensemble de training : 13.42%
+    \n - MAPE sur l’ensemble de test : 12.67%
+    \n - sMAPE sur l’ensemble de test : 12.22%
+    """
+    st.write(text)
 
+    image_path = "img/image094.jpg" 
+    st.image(image_path, width=1000)
 
-image_path = "img/image095.png" 
-st.image(image_path, width=1000)
+    st.subheader("Tuning des Hyperparamètres et performances finales")
 
-text = """
-Après tuning, on observe une convergence de la fonction Loss vers 0, équivalente entre l’ensemble de validation et d’entrainement.
-Nous obtenons les valeurs suivantes qui sont très satisfaisantes sur les ensembles de training et de test, avec une légère amélioration :
-\n - MAPE sur l’ensemble de training : 13.19%
-\n - sMAPE sur l’ensemble de training : 12.47%
-\n - MAPE sur l’ensemble de test : 11.90%
-\n - sMAPE sur l’ensemble de test : 11.53%
-\n
-Les prédictions effectuées sur les ensembles de training et de test sont légèrement améliorées, avec des valeurs pour les pics plus élevés mais toujours plus faibles que les valeurs réelles.
-"""
-st.write(text)
+    text = """
+    Nous avons utilisé le dictionnaire ci-dessous de paramètres pour effectuer le tuning des hyperparamètres :
+    """
+    st.write(text)
 
 
-image_path = "img/image096.png" 
-st.image(image_path, width=600)
+    image_path = "img/image095.png" 
+    st.image(image_path, width=1000)
 
-text = """
-Le tuning permet d'obtenir des prédictions légèrement améliorées, avec des valeurs pour les pics plus élevés mais toujours plus faibles que les valeurs réelles.
-"""
-st.write(text)
-
-
-image_path = "img/image097.jpg" 
-st.image(image_path, width=1000)
-
-
-text = """
-L’écart relatif entre le nombre d’accidents prédit par le modèle et le nombre d’accidents réels sur l’année 2023 est très faible, de 0.6 %, ce qui est excellent. 
-Cela répond pleinement à notre objectif, et le modèle est largement suffisant pour prédire le nombre d’accidents moyens annuels, où la prédiction des pics n’est pas déterminante.
-"""
-st.write(text)
+    text = """
+    On obtient comme meilleurs paramètres principaux un nombre de couches de 2, un taux de dropout de 0.3 et une activation par la fonction tanh. 
+    Après tuning avec ces paramètres, on observe une convergence de la fonction Loss vers 0, équivalente entre l’ensemble de validation et d’entrainement.
+    Nous obtenons les valeurs suivantes qui sont très satisfaisantes sur les ensembles de training et de test, avec une légère amélioration :
+    \n - MAPE sur l’ensemble de training : 13.19%
+    \n - sMAPE sur l’ensemble de training : 12.47%
+    \n - MAPE sur l’ensemble de test : 11.90%
+    \n - sMAPE sur l’ensemble de test : 11.53%
+    \n
+    Les prédictions effectuées sur les ensembles de training et de test sont légèrement améliorées, avec des valeurs pour les pics plus élevés mais toujours plus faibles que les valeurs réelles.
+    """
+    st.write(text)
 
 
+    image_path = "img/image096.png" 
+    st.image(image_path, width=600)
+
+    text = """
+    Le tuning permet d'obtenir des prédictions légèrement améliorées, avec des valeurs pour les pics plus élevés mais toujours plus faibles que les valeurs réelles.
+    """
+    st.write(text)
+
+
+    image_path = "img/image097.jpg" 
+    st.image(image_path, width=1000)
+
+
+    text = """
+    L’écart relatif entre le nombre d’accidents prédit par le modèle et le nombre d’accidents réels sur l’année 2023 est très faible, de 0.6 %, ce qui est excellent. 
+    Cela répond pleinement à notre objectif, et le modèle est largement suffisant pour prédire le nombre d’accidents moyens annuels, où la prédiction des pics n’est pas déterminante.
+    """
+    st.write(text)
+
+    return
+
+tab1, tab2 = st.tabs(
+        ["Approche", "Résultats"]
+    )
+
+with tab1:
+        tab_approche()
+
+with tab2:
+        tab_resultats()
--- 5_modelisation_nbacc/cas_usage_nbacc.py
+++ 5_modelisation_nbacc/cas_usage_nbacc.py
@@ -12,7 +12,10 @@ text = """
 La prévision du nombre total des accidents permettrait de demander, de justifier et de mettre en place les moyens nécessaires (optimiser l’affectation et la mobilisation des forces de l’ordre sur l’ensemble du territoire, développer des campagnes de sensibilisation et de dissuasion *, …) pour répondre aux directives de la CEE.
 L’image ci-dessous représente un exemple de mobilisation des forces de l’ordre en terme humain (gendarmes sur le terrain) et matériel (véhicules de gendarmerie).
 Dans un contexte de réduction des dépenses, la prédiction du volume annuel d’accidents rentrerait dans le cadre de la préparation du budget et de son optimisation argumentée.
+\n
 *lutte contre la consommation d’alcool et de stupéfiants, utilisation du téléphone portable au volant, limitation de vitesse, contrôle de vitesse, contrôle du taux d’alcoolémie, …
+\n 
+Nos objectifs sont de concevoir un modèle de type « times-series » en utilisant le Deep-Learning LSTM, évaluer sa performance et de prédire l’évolution du nombre d’accidents en fonction du temps
 """
 
 write_text(text)
@@ -20,11 +23,4 @@ write_text(text)
 image_path = "img/cas_usage_2.jpg" 
 st.image(image_path, width=400)
 
-
-text = """ 
-Nos objectifs sont les suivants :
-\n - Concevoir un modèle de type « times-series » en utilisant le Deep-Learning LSTM et évaluer sa performance
-\n - Prédire l’évolution du nombre d’accidents en fonction du temps
-"""
-
-st.markdown(text)
+st.info("Pour rappel, la CEE a pour objectif de diminuer de moitié le nombre de morts et de blessés graves sur les routes pour 2030, et à le réduire à zéro ou presque à l'horizon 2050, et la France doit s’aligner sur ces objectifs.", icon="ℹ️")
