c8ecc9f16c3f7d9c373358954b3c00d54777292e
vkomiwes <vincent.komiwes@gmail.com>
2025-04-01 18:20:30 +0200

    Add files via upload

--- 1_introduction/introduction.py
+++ 1_introduction/introduction.py
@@ -18,15 +18,15 @@ def tab_contexte_enjeux():
     text = """Un accident de la route est une collision non intentionnelle, qui a lieu sur le réseau routier entre un engin roulant (automobile, camion, moto, vélo, …) 
         et tout autre véhicule, personne ou animal, se déplaçant ou non, engendrant au moins des dégâts matériels, voire des traumatismes ou le décès d'une ou plusieurs personnes impliquées. 
         La CEE a pour objectif de diminuer de moitié le nombre de morts et de blessés graves sur les routes pour 2030, et à le réduire à zéro ou presque à l'horizon 2050. La France devant s’aligner sur ces objectifs, 
-        les enjeux associées sont donnés ci-après, suivants les Ministères :
+        les enjeux associés sont donnés ci-après, suivants les Ministères :
 \n
 1.	Pour le Ministère de la santé, 
  
-L’enjeux est de connaître au plus tôt si les victimes d’ accidents sont blessées graves pour optimiser la prise en charge (transport de la victime, 1er soins, …), la qualité et le délai du traitement pour mettre tout en œuvre pour les sauver. 
+L’enjeu est de connaître au plus tôt si les victimes d’ accidents sont blessées graves pour optimiser la prise en charge (transport de la victime, 1er soins, …), la qualité et le délai du traitement pour mettre tout en œuvre pour les sauver. 
 \n
 2.	Pour le Ministère de l’intérieur,
  
-L’enjeux est de prédire le nombre total des accidents pour justifier et mettre en place les moyens nécessaires (optimiser l’affectation et la mobilisation des forces de l’ordre sur l’ensemble du territoire, développer des campagnes de sensibilisation et de dissuasion *, …) pour répondre aux directives de la CEE.
+L’enjeu est de prédire le nombre total des accidents pour justifier et mettre en place les moyens nécessaires (optimiser l’affectation et la mobilisation des forces de l’ordre sur l’ensemble du territoire, développer des campagnes de sensibilisation et de dissuasion *, …) pour répondre aux directives de la CEE.
 Les cas d’usages métiers sont détaillés dans les parties de modélisation.
 
 
